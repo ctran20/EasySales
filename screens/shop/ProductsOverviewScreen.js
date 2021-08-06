41c4c8667b0a9b794731124e0adc362939fc2d39
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-08-05 21:15:17 -0400

    Added basic styles to item cards

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList } from 'react-native';
 import { useSelector } from 'react-redux';
+import ProductItem from '../../components/shop/ProductItem';
+
 
 const ProductsOverviewScreen = (props) => {
     const products = useSelector(state => state.products.availableProducts);
     return <FlatList
         data={products}
-        renderItem={itemData => <Text>{itemData.item.title}</Text>}
+        renderItem={itemData =>
+            <ProductItem
+                image={itemData.item.imageUrl}
+                title={itemData.item.title}
+                price={itemData.item.price}
+                onViewDetail={() => { }}
+                onAddToCart={() => { }}
+            />}
     />
 };
 
