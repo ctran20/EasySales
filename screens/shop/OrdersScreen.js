b292231c2bcf9f2d9ec6add280a9931cb0f67f10
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-08-17 22:22:11 -0400

    Added Cart Item Info

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -3,6 +3,7 @@ import { Text, FlatList, StyleSheet, Platform } from 'react-native';
 import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
+import OrderItem from '../../components/shop/OrderItem';
 
 const OrdersScreen = (props) => {
   const orders = useSelector((state) => state.orders.orders);
@@ -10,7 +11,12 @@ const OrdersScreen = (props) => {
   return (
     <FlatList
       data={orders}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+        />
+      )}
     />
   );
 };
