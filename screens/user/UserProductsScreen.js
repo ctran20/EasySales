aefc37253814d3bb141e5b68a3a2ac0ee2c617d0
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-08-18 21:13:52 -0400

    Added edit and delete buttons

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View, FlatList, Button, Platform, StyleSheet } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProductItem from '../../components/shop/ProductItem';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
 import Colors from '../../constants/Colors';
+import * as productsActions from '../../store/actions/product';
 
 const UserProductsScreen = () => {
   const userProducts = useSelector((state) => state.products.userProducts);
+  const dispatch = useDispatch();
   return (
     <FlatList
       data={userProducts}
@@ -22,7 +24,13 @@ const UserProductsScreen = () => {
             <Button color={Colors.extra} title="Edt" onPress={() => {}} />
           </View>
           <View style={styles.buttonContainer}>
-            <Button color={'red'} title="Delete" onPress={() => {}} />
+            <Button
+              color={'red'}
+              title="Delete"
+              onPress={() => {
+                dispatch(productsActions.deleteProduct(itemData.item.id));
+              }}
+            />
           </View>
         </ProductItem>
       )}
--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,6 +1,7 @@
 import CartItem from '../../models/cart';
 import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/cart';
 import { ADD_ORDER } from '../actions/orders';
+import { DELETE_PRODUCT } from '../actions/product';
 
 const initialState = {
   items: {},
@@ -61,6 +62,19 @@ export default (state = initialState, action) => {
       };
     case ADD_ORDER:
       return initialState;
+    case DELETE_PRODUCT:
+      if (!state.items[action.pid]) {
+        return state;
+      }
+
+      const updatedItems = { ...state.items };
+      const itemTotal = state.items[action.pid].sum;
+      delete updatedItems[action.pid];
+      return {
+        ...state,
+        items: updatedItems,
+        totalAmount: state.totalAmount - itemTotal,
+      };
     default:
       return state;
   }
