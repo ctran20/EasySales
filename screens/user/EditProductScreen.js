70180d86526913184c7185a359e7ef1224a4a9e2
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-08-18 21:39:14 -0400

    Added add product button

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, ScrollView, Text, TextInput, StyleSheet } from 'react-native';
 
 const EditProductScreen = () => {
   return (
-    <View>
-      <Text>Hello EditProductScreen!</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Image URL</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Price</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput style={styles.input} />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
